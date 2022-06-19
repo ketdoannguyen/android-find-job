fddced916cf2e5f67b8a22fc29569c38ba0f3a96
Duc <Kidkender@gmail.com>
2022-06-19 23:22:19 +0700

    Ken

--- app/src/main/java/com/android/app_findjob/Admin_AddJob_Activity.java
+++ app/src/main/java/com/android/app_findjob/Admin_AddJob_Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.android.app_findjob.databinding.ActivityAdminAddEmployerBinding;
 import com.android.app_findjob.databinding.ActivityAdminAddJobBinding;
@@ -27,8 +28,12 @@ public class Admin_AddJob_Activity extends AppCompatActivity {
         binding = ActivityAdminAddJobBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getListDataFirebase();
 
+    }
 
+    private void getListDataFirebase() {
     }
 
 }
